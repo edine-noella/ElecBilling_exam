25e8931e0efa0992e721057830a63d4a5e9ad9f3
Gahamanyi-dev <ygahamanyi26@gmail.com>
2022-03-24 13:23:25 +0200

    updated project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,6 @@ import app from "./utils/app";
 
 const server = app();
 
-server.listen(3000, () => {
-  console.log("Server started on port 3000");
+server.listen(4000, () => {
+  console.log("Server started on port 4000");
 });
